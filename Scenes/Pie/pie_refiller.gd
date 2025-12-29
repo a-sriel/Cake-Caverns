2cab17cbeed2ba90cf68e9e1f04f726ed80be500
ColinNeary <colinneary2749@gmail.com>
2025-12-29 01:26:36 -0600

    boss arena refillers respawn

--- Scenes/Pie/pie_refiller.gd
+++ Scenes/Pie/pie_refiller.gd
@@ -1,13 +1,31 @@
 extends RigidBody3D
 
 @export var amount : int = 25
+@export var in_boss_arena : bool = false
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is CakeMaster:
 		body.refill_pies(amount)
 		amount *= .5
-		self.scale *= .7
+		self.scale *= .8
 		
 		if amount <= 0:
-			self.queue_free()
+			if not in_boss_arena:
+				self.queue_free()
+			
+			disable_self()
+
+func disable_self() -> void:
+	self.visible = false
+	call_deferred("set_process", PROCESS_MODE_DISABLED)
+	$"Respawn Timer".start()
+
+func enable_self() -> void:
+	amount = 25
+	self.scale = Vector3.ONE
+	self.visible = true
+	call_deferred("set_process", PROCESS_MODE_INHERIT)
+
+func _on_respawn_timer_timeout() -> void:
+	enable_self()
